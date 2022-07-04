9c2ac5dda2523f20c128d208ad0a3eacc96546a2
Andrew Hendry <aahendry@hotmail.com>
2022-07-04 21:22:34 +0100

    Update daily_mail.py

--- daily_mail.py
+++ daily_mail.py
@@ -11,12 +11,9 @@ env.read_env()
 
 DM_BIRTHDAY_FILE = env('DM_BIRTHDAY_FILE')
 DM_GITHUB_EDIT = env('DM_GITHUB_EDIT')
-DM_FINNHUB_TOKEN = env('DM_FINNHUB_TOKEN')
 DM_SENDGRID_TOKEN = env('DM_SENDGRID_TOKEN') 
 DM_FROM_EMAIL = env("DM_FROM_EMAIL")
 DM_TO_EMAILS  = env.list("DM_TO_EMAILS")
-DM_IEX_TOKEN = env('DM_IEX_TOKEN')
-DM_IEX_SERVER = env('DM_IEX_SERVER')
 DM_XDAYS = env.int('DM_XDAYS')
 
 def load_csvfile(filename):
@@ -59,44 +56,8 @@ def get_birthday_html():
     html += f'<br><a href="{DM_GITHUB_EDIT}">Edit</a><br>'
     return html
 
-def get_stock_html ():
-    print('=====get_stock_html====')
-    html = ''
-    try:
-        r = requests.get(f'{DM_IEX_SERVER}/stable/stock/voo/quote?token={DM_IEX_TOKEN}')
-        d = r.json()
-        print(r.status_code)
-        print(r.headers)
-        price_old = d["previousClose"]
-        price_now = d["latestPrice"]
-        price_diff = (price_now - price_old) * 100/ price_old
-        html += f'<code>Vanguard VOO = {price_now}&nbsp;{price_diff:+.2f}%</code>'
-    except:
-        html += 'Error getting stock_prices!! <br>'
-    return html
-
-def get_forex_html ():
-    print('=====get_forex_html====')
-    html = ''
-    try:
-        r = requests.get(f'https://finnhub.io/api/v1/forex/rates?token={DM_FINNHUB_TOKEN}')
-        print(r.status_code)
-        print(r.headers)
-        d = r.json()
-        html += '<code>'
-        html += f'1 EUR = {d["quote"]["ZAR"]:.2f} ZAR<br>'
-        html += f'1 USD = {d["quote"]["ZAR"]/d["quote"]["USD"]:.2f} ZAR<br>'
-        html += f'1 EUR = {d["quote"]["USD"]:.3f} USD<br>'
-        html += f'1 GBP = {d["quote"]["USD"]/d["quote"]["GBP"]:.3f} USD<br>'
-        html += '</code>'
-    except:
-        html += 'Error getting forex!! <br>'
-    return html
-
 def main():
     html_content = \
-        get_forex_html() + \
-        get_stock_html() + \
         get_birthday_html()
     
     now = datetime.datetime.now() 
